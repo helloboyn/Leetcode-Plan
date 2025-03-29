a9ee33ea1e39fafa984e85843c4f34fca13aa82a
Boyn Francis <boynfrancis@gmail.com>
2025-03-30 02:40:23 +0530

    Update 1. 1768. Merge Strings Alternately.py

--- LeetCode 75/1. Array String/1. 1768. Merge Strings Alternately.py
+++ LeetCode 75/1. Array String/1. 1768. Merge Strings Alternately.py
@@ -1,3 +1,4 @@
+https://leetcode.com/problems/merge-strings-alternately/description/?envType=study-plan-v2&envId=leetcode-75
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
         i, j = 0, 0 # Initialize two pointers for word1 and word2
